a721376d4e288f9967faf3079c6f38f70b4cd55d
ts3486 <taos3486@gmail.com>
2021-03-02 14:49:10 +0900

    commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Heroku Project</h1>
+      <div>Trying to upload to heroku</div>
+    </div>
+  );
+}
+
+export default App;
